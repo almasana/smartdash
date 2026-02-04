dd3cb2df914cc132d005af7701d0490095f59137
Brigimar <160976236+brigimar@users.noreply.github.com>
2026-02-04 12:35:31 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import './globals.css';
-import { ThemeProvider } from '@/ui/theme-provider';
+import { ThemeProvider } from '@/theme-provider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
