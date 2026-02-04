f1f0c0790370e2c2d32ce3921f3cbb8c528c696d
Brigimar <160976236+brigimar@users.noreply.github.com>
2026-02-04 13:17:21 -0300

    Refactor Avatar component styles and classes

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
-
 import { cn } from "@/lib/utils";
 
 const Avatar = React.forwardRef<
@@ -12,8 +11,8 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className,
+      "relative flex shrink-0 overflow-hidden rounded-full bg-card",
+      className
     )}
     {...props}
   />
@@ -26,7 +25,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn("h-full w-full object-cover", className)}
     {...props}
   />
 ));
@@ -39,8 +38,8 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
-      className,
+      "flex h-full w-full items-center justify-center rounded-full bg-muted text-muted-foreground",
+      className
     )}
     {...props}
   />
